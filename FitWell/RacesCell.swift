83342ea1bdcd4637d327cd8772324843caacd935
Lefteris <AnoxySoftware@users.noreply.github.com>
2018-07-09 11:45:56 +0300

    Initial Commit

---
+++ FitWell/RacesCell.swift
@@ -0,0 +1,20 @@
+//
+//  RacesCell.swift
+//  FitWell
+//
+//  Created by Lefteris Haritou on 09/07/2018.
+//  Copyright © 2018 Lefteris Haritou. All rights reserved.
+//
+
+import UIKit
+
+class RacesCell : UITableViewCell {
+    
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var detailArrow: UIImageView!
+    @IBOutlet weak var raceLenghtLabel: UILabel!
+    @IBOutlet weak var userRacesLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+}
---
+++ FitWell/RacesVC.swift
@@ -0,0 +1,25 @@
+//
+//  RacesVC.swift
+//  FitWell
+//
+//  Created by Lefteris Haritou on 09/07/2018.
+//  Copyright © 2018 Lefteris Haritou. All rights reserved.
+//
+
+import UIKit
+
+class RacesVC : UIViewController {
+    
+    
+}
+
+extension RacesVC : UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return nil
+    }
+    
+}
